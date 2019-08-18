d2382dd5d442848005861a7f093b173cff5b67a1
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-18 17:02:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do 
+    @user_input = params[:user_phrase]
     erb :user_input 
   end 
   
   post '/piglatinize' do
-    @user_input = Piglatinizer.new(params[:user_phrase])
+    #@user_input = Piglatinizer.new(params[:user_phrase])
     erb :piglatinize
   end 
 end
